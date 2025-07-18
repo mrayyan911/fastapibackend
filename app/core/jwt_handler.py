fc435682113adb69f96575d9c0112331786dad87
mrvibedsc <rayyanvibe4072587@gmail.com>
2025-07-18 23:30:32 +0500

    Add jwt dependency file

---
+++ app/core/jwt_handler.py
@@ -0,0 +1,20 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from typing import Optional
+import os
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
